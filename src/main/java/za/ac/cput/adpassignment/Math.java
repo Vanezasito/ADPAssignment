ee1c5d348cb1f94bb6bb860620788ac244568e4e
Mpumelelo Magagula <218136021@mycput.ac.za>
2021-03-31 23:26:49 +0200

    adding my MathTest my final commit with testing

--- src/main/java/za/ac/cput/adpassignment/Math.java
+++ src/main/java/za/ac/cput/adpassignment/Math.java
@@ -8,15 +8,41 @@ public class Math {
      * @param c
      * @param d
      */
-    public static int sum(final int c, final int d){
+//    public static int sum(final int c, final int d){
+//
+//        return c + d;
+//    }
+//    public static double dublesum(double c, double d){
+//        return c + d;
+//    }
+//    public static void main(String[] args){
+//
+//        System.out.println(sum( 2,6));
+//    }
+    private int c, d;
 
-        return c + d;
+    public int getC() {
+        return c;
     }
-    public static double dublesum(double c, double d){
-        return c + d;
-    }
-    public static void main(String[] args){
 
-        System.out.println(sum( 2,6));
+    public int getD() {
+        return d;
     }
+
+    public void setC(int c) {
+        this.c = c;
+    }
+
+    public void setD(int d) {
+        this.d = d;
+    }
+
+    @Override
+    public String toString() {
+        return "Math{" +
+                "c=" + c +
+                ", d=" + d +
+                '}';
+    }
+
 }

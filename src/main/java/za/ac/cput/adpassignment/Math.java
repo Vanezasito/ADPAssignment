63af245ac79752ea8ac95543e9e02546edfd975f
Mpumelelo Magagula <218136021@mycput.ac.za>
2021-03-31 22:00:04 +0200

    my first branch

---
+++ src/main/java/za/ac/cput/adpassignment/Math.java
@@ -0,0 +1,20 @@
+package za.ac.cput.adpassignment;
+
+public class Math {
+    /**
+     *this is a division of teo integer program that returns a results
+     * @author : Mpumelelo Magagula
+     *  Student no: 218136021
+     * @param c
+     * @param d
+     */
+    public static int sum(final int c, final int d){
+
+        return c /d;
+
+    }
+    public static void main(String[] args){
+
+        System.out.println(sum( 2,6));
+    }
+}
